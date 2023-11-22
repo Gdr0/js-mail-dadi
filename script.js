c701c08db53d42542747f87952f739c092d36bf9
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2023-11-22 18:49:12 +0100

    dadi in html

--- script.js
+++ script.js
@@ -7,38 +7,6 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // stilo una lisa delle mail
-// const mailList = [
-//   "jotaro@jmail.com",
-//   "joseph@jmail.com",
-//   "polnareff@jmail.com",
-//   "kakyoin@jmail.com",
-//   "abdul@jmail.com",
-// ];
-
-// const container = document.querySelector(".container");
-// // creo l'evento per verificare la mail
-// const ricerca = document.querySelector(".bottone-verifica");
-// ricerca.addEventListener("click", function () {
-//   // acquisisco la mail dell'utente
-//   const userMail = document.querySelector(".mail-user");
-//   let mail = userMail.value;
-//   console.log(mail);
-
-//   // controllo che la mial sia nella lista
-//   if (mailList.includes(mail)) {
-//     const verifica = `<h1> La tua mail è stata verificata!</h1>`;
-//     container.innerHTML += verifica;
-
-//     //   se presente stampo il messaggio di conferma
-//     console.log(" La tua mail è stata verificata!");
-//   } else {
-//     // se non è presente informo l'utente
-//     console.log("siamo spiacenti, non riusciamo a trovare la tua mail...");
-//     const nonVer = `<h1>siamo spiacenti, non riusciamo a trovare la tua mail...</h1>`;
-//     container.innerHTML += nonVer;
-//   }
-// });
-
 const mailList = [
   "jotaro@jmail.com",
   "joseph@jmail.com",
@@ -46,6 +14,38 @@ const mailList = [
   "kakyoin@jmail.com",
   "abdul@jmail.com",
 ];
+
+const container = document.querySelector(".container");
+// creo l'evento per verificare la mail
+const ricerca = document.querySelector(".bottone-verifica");
+ricerca.addEventListener("click", function () {
+  // acquisisco la mail dell'utente
+  const userMail = document.querySelector(".mail-user");
+  let mail = userMail.value;
+  console.log(mail);
+
+  // controllo che la mial sia nella lista
+  if (mailList.includes(mail)) {
+    const verifica = `<h1> La tua mail è stata verificata!</h1>`;
+    container.innerHTML += verifica;
+
+    //   se presente stampo il messaggio di conferma
+    console.log(" La tua mail è stata verificata!");
+  } else {
+    // se non è presente informo l'utente
+    console.log("siamo spiacenti, non riusciamo a trovare la tua mail...");
+    const nonVer = `<h1>siamo spiacenti, non riusciamo a trovare la tua mail...</h1>`;
+    container.innerHTML += nonVer;
+  }
+});
+
+// const mailList = [
+//   "jotaro@jmail.com",
+//   "joseph@jmail.com",
+//   "polnareff@jmail.com",
+//   "kakyoin@jmail.com",
+//   "abdul@jmail.com",
+// ];
 
 // for (let i = 0; i < mailList.length; i++) {
 //   let mailL1 = mailList[i];
@@ -58,17 +58,30 @@ const mailList = [
 // }
 
 // dadi
-// risultato computer
-computer = Math.floor(Math.random() * 6) + 1;
-console.log(computer);
-// risultato gioctore
-giocatore = Math.floor(Math.random() * 6) + 1;
-console.log(giocatore);
+// creo costante per container dadi
+const containerDadi = document.querySelector(".container-dadi");
+// creo comando per l'evento
+const sfida = document.querySelector(".genera");
+sfida.addEventListener("click", function () {
+  // risultato computer
+  computer = Math.floor(Math.random() * 6) + 1;
+  console.log(computer);
 
-if (computer > giocatore) {
-  console.log("computer ha vinto!");
-} else if (computer < giocatore) {
-  console.log("giocatore ha vinto");
-} else if ((computer = giocatore)) {
-  console.log("Pareggio!");
-}
+  // risultato gioctore
+  giocatore = Math.floor(Math.random() * 6) + 1;
+  console.log(giocatore);
+  // confronto i risultati e li stampo
+  if (computer > giocatore) {
+    console.log("computer ha vinto!");
+    const pcWin = `<h1> Computer ha vinto!</h1>`;
+    containerDadi.innerHTML += pcWin;
+  } else if (computer < giocatore) {
+    console.log("giocatore ha vinto!");
+    const humanWin = `<h1> Giocatore ha vinto!</h1>`;
+    containerDadi.innerHTML += humanWin;
+  } else if ((computer = giocatore)) {
+    console.log("Pareggio!");
+    const pari = `<h1> Pareggio!</h1>`;
+    containerDadi.innerHTML += pari;
+  }
+});
